b5f31cc997d5d99a746b416df45ca2ff55c16210
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2022-08-27 19:45:40 +0200

    idb

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,35 @@
-<script>
-    import { onMount } from 'svelte';
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { openDB, deleteDB, wrap, unwrap } from 'idb';
+	import type { Todo } from '../lib/types';
 
-    onMount(() => {
-		console.log('the component has mounted');
+  let data: Todo[] = [];
+
+	onMount(async () => {
+
+    // await deleteDB("foo.db");
+
+		const db = await openDB('foo.db', 1, {
+			upgrade(db) {
+				db.createObjectStore('todos', { 
+          keyPath: "id", 
+          autoIncrement: true 
+        });
+			}
+		});
+
+    // if(!db.objectStoreNames.contains("") == false){
+    //   db.createObjectStore('todos', { keyPath: "id", autoIncrement: true });
+    // }
+
+		let obj: Todo = { title: '', description: '', done: false };
+
+		let added = await db.add('todos', obj);
+
+    data = await db.getAll("todos") as Todo[];
 	});
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>{data.length}</h2>
