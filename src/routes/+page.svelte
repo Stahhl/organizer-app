ac5c20aadf018b3040745283883b78970b2463bd
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2022-08-27 19:49:30 +0200

    formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,30 +3,25 @@
 	import { openDB, deleteDB, wrap, unwrap } from 'idb';
 	import type { Todo } from '../lib/types';
 
-  let data: Todo[] = [];
+	let data: Todo[] = [];
 
 	onMount(async () => {
+		// await deleteDB("todo.db");
 
-    // await deleteDB("foo.db");
-
-		const db = await openDB('foo.db', 1, {
+		const db = await openDB('todo.db', 1, {
 			upgrade(db) {
-				db.createObjectStore('todos', { 
-          keyPath: "id", 
-          autoIncrement: true 
-        });
+				db.createObjectStore('todos', {
+					keyPath: 'id',
+					autoIncrement: true
+				});
 			}
 		});
 
-    // if(!db.objectStoreNames.contains("") == false){
-    //   db.createObjectStore('todos', { keyPath: "id", autoIncrement: true });
-    // }
-
 		let obj: Todo = { title: '', description: '', done: false };
 
-		let added = await db.add('todos', obj);
+		await db.add('todos', obj);
 
-    data = await db.getAll("todos") as Todo[];
+		data = (await db.getAll('todos')) as Todo[];
 	});
 </script>
 
