da3b3005c56aa69e340d89c2ea5e65dfd0b90d56
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2022-09-17 16:03:14 +0200

    basic layout

---
+++ src/components/ListItem.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Todo } from '$lib/types';
+
+	export let todo: Todo;
+</script>
+
+<div class="shadow-m bg-teal-200">
+	<div class="m-1">
+		{todo.title} <br>
+		{todo.description}
+	</div>
+</div>
+ 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,12 @@
 	import { onMount } from 'svelte';
 	import { openDB, deleteDB, wrap, unwrap } from 'idb';
 	import type { Todo } from '../lib/types';
+	import ListItem from '../components/ListItem.svelte';
 
 	let data: Todo[] = [];
 
 	onMount(async () => {
-		// await deleteDB("todo.db");
+		//await deleteDB('todo.db');
 
 		const db = await openDB('todo.db', 1, {
 			upgrade(db) {
@@ -17,7 +18,7 @@
 			}
 		});
 
-		let obj: Todo = { title: '', description: '', done: false };
+		let obj: Todo = { title: 'hej', description: 'värld', done: false };
 
 		await db.add('todos', obj);
 
@@ -25,6 +26,29 @@
 	});
 </script>
 
-<h1 class="font-medium">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h2>Todos: {data.length}</h2>
+<div class="flex flex-col h-screen m-1">
+	<div class="bg-red-500 text-center">
+		Hello World
+	</div>
+	<div class="flex-1 overflow-y-auto">
+		{#each data as todo}
+			<ListItem bind:todo />
+		{/each}
+	</div>
+	<div class="bg-blue-500 text-center h-10 mb-2">
+		<button>Add Todo</button>
+	</div>
+</div>
+
+<!-- <div class="row-span-1">
+	<h1 class="font-medium">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<h2>Todos: {data.length}</h2>
+
+	{#each data as todo}
+		<ListItem bind:todo />
+	{/each}
+</div>
+<div class="row-span-1">
+	<button>hej</button>
+</div> -->
